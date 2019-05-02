78a2b13309c962d69d22edaa4d309f67fac6e1d0
Dennis <d.loska@hotmail.de>
2019-05-02 13:09:17 +0200

    issue: adding babel presets to support ES7 JSX syntax

--- app/src/App.js
+++ app/src/App.js
@@ -3,10 +3,13 @@ import Home from './components/Home';
 import './App.css';
 
 class App extends Component {
+  state = {
+    babelTest: true
+  };
 
-  constructor(props) {
-    super(props);
-  }
+  testBabel = () => {
+    console.log('test');
+  };
 
   render() {
     return (
