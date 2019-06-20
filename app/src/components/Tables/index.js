30a7c1547db2d27a7ffb5090f5bb4d148a59c5ca
Elsa Schroeder <elsa.schroeder@cimt-ag.de>
2019-06-20 20:55:18 +0200

    set up tables

--- app/src/components/Tables/index.js
+++ app/src/components/Tables/index.js
@@ -5,9 +5,9 @@ const Tables = (props) => {
   return (
     <React.Fragment>
       <h1> An welchem Tisch sitzt du?</h1>
-      <div className="tables" >
+      <div className="d-sm-flex flex-row justify-content-center align-content-stretch flex-wrap" >
         {props.names.map(name =>
-          <div key={name}>{name}</div>
+          <div key={name} className="table"><p className="tableNames">{name}</p></div>
         )}
       </div>
     </React.Fragment>);
