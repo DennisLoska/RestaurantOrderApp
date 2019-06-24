b879fd307dea61bb3619cabcb978329dfdc2ea71
Dennis <d.loska@hotmail.de>
2019-06-24 18:19:02 +0200

    feature: added fetch() request to get menu items from the server

--- app/src/components/TableMenu/index.js
+++ app/src/components/TableMenu/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import MenuCard from './MenuCard';
 import OrderHistory from './OrderHistory';
@@ -10,22 +10,35 @@ import Tabs from '../Tabs';
  *
  * @param {Object} props Properties
  * @param {{path: string}} props.match The path pattern used to match
- * @param {{id: string, image: string, food: string, price: string}[]} props.menus Current orders for this table
+ * @param {{id: string, image: string, food: string, price: string}[]} props.orders Current orders for this table
+ * @param {{ _id: object, img_url: string, name: string, price: string, description: string, category: string }[]} props.menus List of food options
  * @param {{id: string, date: string, food: string}} props.history Previous orders
  */
 const TableMenu = props => {
   const {
     match: { path },
-    menus,
     history,
     orders
   } = props;
+
+  const [items, setItems] = useState({ items: null });
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/items')
+      .then(response => response.json())
+      .catch(err => console.log(err))
+      .then(items => {
+        setItems(items);
+        console.log(items);
+      })
+      .catch(err => console.log(err));
+  }, [items === null]);
 
   return (
     <div id="container">
       <section id="menuOptions">
         <Tabs>
-          <MenuCard to={path} label="Menu Card" menus={menus} />
+          <MenuCard to={path} label="Menu Card" menus={items} />
           <OrderHistory
             to={`${path}/history`}
             label="Order History"
