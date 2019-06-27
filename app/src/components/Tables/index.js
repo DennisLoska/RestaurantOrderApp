1f412960a13be762a621a66cf113e4d1123b1867
Elsa Schroeder <elsa.schroeder@cimt-ag.de>
2019-06-27 12:50:52 +0200

    changed tables format

--- app/src/components/Tables/index.js
+++ app/src/components/Tables/index.js
@@ -6,8 +6,12 @@ const Tables = (props) => {
     <React.Fragment>
       <h1> An welchem Tisch sitzt du?</h1>
       <div className="d-sm-flex flex-row justify-content-center align-content-stretch flex-wrap" >
-        {props.names.map(name =>
-          <div key={name} className="table"><p className="tableNames">{name}</p></div>
+        {props.names.map(name => {
+          const handleOnClick = () => {
+            alert(name);
+          };
+          return <div key={name} className="table" onClick={handleOnClick}><p className="tableNames">{name}</p></div>;
+        }
         )}
       </div>
     </React.Fragment>);
